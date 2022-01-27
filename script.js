926c99731d5f850ee127016e75f8b4a0cdf0d30a
emilssats <emilio268xoxo@gmail.com>
2022-01-27 14:38:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const cilv = ["Aivars Bērziņš", "Gunārs janovskis", "Jānis Krūmiņš", "Kārlis Maska", "Ivans Timoshenko"];
-const balva = ["Dators ar Ryzen 9", "SAMSUNG A70", "McLaren P1", "Mercedes-Benz AMG GT Black Series", "GOOGLE"];
+const cilv = ["ivars Bērziņš", "Gunis janovskis", "Jānis Krumins", "Kārlis Maskačka", "Ivans Timotejs"];
+const balva = ["Dators ar Ryzen ugunskuru", "SAMSUNG A420", "McLaren BEN15", "Mercedes-Benz AMG", "GOOGLE stonks"];
 const naudaKopa=100000;//kopeja summa
 let uzvaretajuSkaits=5;//uzvarētāju skaits
 let rand= Math.random()*5;
 rand=Math.floor(rand);//noapaļo uz leju, math.floor - noapaļo uz leju
 console.log(cilv[rand]);//izvada konsolē
 
+for(let i=0;<uzvaretajuSkaits;i++){
+    let rand =Math.random()*vardi.length;
+    rand = Math.floor(rand);
+    let uzvaretajs = vardi[rand];
+    rindas.innerHTML +=
+
+<tr>
+    <td>${i+1}</td>
+    <td>${uzvaretajs</td>
+</tr>
+}
