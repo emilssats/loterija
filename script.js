03bbfb1b636aa27404998918f9c340d1ee47fad0
emilssats <emilio268xoxo@gmail.com>
2022-02-17 14:35:04 +0200

    loterija

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const cilv = ["ivars Bērziņš", "Gunis janovskis", "Jānis Krumins", "Kārlis Maskačka", "Ivans Timotejs"];
-const balva = ["Dators ar Ryzen ugunskuru", "SAMSUNG A420", "McLaren BEN15", "Mercedes-Benz AMG", "GOOGLE stonks"];
+const cilv = ["Jānis lapsa", "uvis Gubers", "Artūrs pērkons ", "Igors Timians", "Ivans sergejsons"];
+const balva = ["Dators", "SAMSUNG telefons", "lamborgini", "NFT kolekcija", "GOOGLE"];
 const naudaKopa = 100000;//kopeja summa
 let uzvaretajuSkaits = 5;//uzvarētāju skaits
-let rindas = document.querySelector('.rindas');
-rindas.innerHTML = '';
+let rindas = document.querySelector(".rindas");
+rindas.innerHTML = " ";
 
 for (let i = 0; i < uzvaretajuSkaits; i++) {
+    //let rand = Math.random()*vardi.length;
     let rand = Math.random() * 5;
-    rand = Math.floor(rand);
+    rand = Math.floor(rand);//noapaļo uz leju, math.floor - noapaļo uz leju
 
+
+    let bonus = balva[rand];
     let uzvaretajs = cilv[rand];
     rindas.innerHTML += `
-    <tr>
-    <td>${i + 1}</td>
-    <td>${uzvaretajs}</td>
-    </tr>`
+<tr>
+<td>${i + 1}</td>
+<td>${uzvaretajs}</td>
+<td>${bonus}</td>
+</tr>`
 }
