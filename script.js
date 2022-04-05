f498fa5605bf68c12ff7629341cdeec41a19a4a9
emilssats <emilio268xoxo@gmail.com>
2022-04-05 18:41:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const cilv = ["Jānis lapsa", "uvis Gubers", "Artūrs pērkons ", "Igors Timians", "Ivans sergejsons"];
-const balva = ["Dators", "SAMSUNG telefons", "lamborgini", "NFT kolekcija", "GOOGLE"];
+const cilv = ["jānis bērziņš", "valdis zatlers", "vaira vīķe freiberga", "raimonds vējonis", "egils levits"];
+const balva = ["velosipēds", "telefons", "mašīna", "ceļojums uz spāniju", "dimants"];
 const naudaKopa = 100000;//kopeja summa
 let uzvaretajuSkaits = 5;//uzvarētāju skaits
 let rindas = document.querySelector(".rindas");
 rindas.innerHTML = " ";
 
 for (let i = 0; i < uzvaretajuSkaits; i++) {
-    //let rand = Math.random()*vardi.length;
     let rand = Math.random() * 5;
-    rand = Math.floor(rand);//noapaļo uz leju, math.floor - noapaļo uz leju
+    rand = Math.floor(rand);//noapaļo uz leju
 
 
     let bonus = balva[rand];
